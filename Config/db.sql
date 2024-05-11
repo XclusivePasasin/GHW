6b08326181c1e9ddcafc82d4affc98c671a29a1c
A_Luna <alondra.luna20@itca.edu.sv>
2024-05-11 13:50:16 -0600

    se ha modificado la base de datos, integrando, foraneas y algunso detalles minimos -->> ver archivo para comprender att:Lifewaver

--- Config/db.sql
+++ Config/db.sql
@@ -33,7 +33,7 @@ CREATE TABLE Category
     TicketCategory varchar(255)
 );
 
-CREATE TABLE _User
+CREATE TABLE User
 (
     ID_User int,
     email varchar(255),
@@ -74,3 +74,88 @@ CREATE TABLE Comment
 ); 
 
 //Hay que insertar los datos de las tablas que no cuentan con llaves foraneas en ellas antes de rellenar las principales.
+-- Modificar la clave foranea en la tabla Ticekt para usar ON UPDATE CASCADE //Status
+    ALTER TABLE Ticket ADD CONSTRAINT fk_Ticket_ID_Status 
+    FOREIGN KEY (ID_Status) REFERENCES Status(ID_Status) 
+    ON UPDATE CASCADE;
+
+-- Modificar la clave foranea en la tabla Ticekt para usar ON DELETE CASCADE
+    ALTER TABLE Ticket ADD CONSTRAINT fk_Ticket_ID_Status 
+    FOREIGN KEY (ID_Status) REFERENCES Status(ID_Status) 
+    ON DELETE CASCADE;
+
+-- Modificar la clave foranea en la tabla Ticekt para usar ON UPDATE CASCADE //User
+    ALTER TABLE Ticket ADD CONSTRAINT fk_Ticket_ID_User 
+    FOREIGN KEY (ID_User) REFERENCES User(ID_User) 
+    ON UPDATE CASCADE;
+
+-- Modificar la clave foranea en la tabla Ticekt para usar ON DELETE CASCADE
+    ALTER TABLE Ticket ADD CONSTRAINT fk_Ticket_ID_User 
+    FOREIGN KEY (ID_User) REFERENCES User(ID_User) 
+    ON DELETE CASCADE;
+
+-- Modificar la clave foranea en la tabla Ticekt para usar ON UPDATE CASCADE //Priority
+    ALTER TABLE Ticket ADD CONSTRAINT fk_Ticket_ID_Priority
+    FOREIGN KEY (ID_Priority) REFERENCES Priority(ID_Priority) 
+    ON UPDATE CASCADE;
+
+-- Modificar la clave foranea en la tabla Ticekt para usar ON DELETE CASCADE
+    ALTER TABLE Ticket ADD CONSTRAINT fk_Ticket_ID_Priority
+    FOREIGN KEY (ID_Priority) REFERENCES Priority(ID_Priority) 
+    ON DELETE CASCADE;
+
+-- Modificar la clave foranea en la tabla Ticekt para usar ON UPDATE CASCADE //Category
+    ALTER TABLE Ticket ADD CONSTRAINT fk_Ticket_ID_Category
+    FOREIGN KEY (ID_Category) REFERENCES Category(ID_Category) 
+    ON UPDATE CASCADE;
+
+-- Modificar la clave foranea en la tabla Ticekt para usar ON DELETE CASCADE
+    ALTER TABLE Ticket ADD CONSTRAINT fk_Ticket_ID_Category
+    FOREIGN KEY (ID_Category) REFERENCES Category(ID_Category) 
+    ON DELETE CASCADE;
+
+
+
+-- Modificar la clave foranea en la tabla User para usar ON UPDATE CASCADE //Employee
+    ALTER TABLE User ADD CONSTRAINT fk_User_ID_Employee 
+    FOREIGN KEY (ID_Employee) REFERENCES Employee(ID_Employee) 
+    ON UPDATE CASCADE;
+
+-- Modificar la clave foranea en la tabla User para usar ON DELETE CASCADE
+    ALTER TABLE User ADD CONSTRAINT fk_User_ID_Employee 
+    FOREIGN KEY (ID_Employee) REFERENCES Employee(ID_Employee) 
+    ON DELETE CASCADE;
+
+
+
+-- Modificar la clave foranea en la tabla Employee para usar ON UPDATE CASCADE //Rol
+    ALTER TABLE Employee ADD CONSTRAINT fk_Employee_ID_Rol 
+    FOREIGN KEY (ID_Rol) REFERENCES Rol(ID_Rol) 
+    ON UPDATE CASCADE;
+
+-- Modificar la clave foranea en la tabla Employee para usar ON DELETE CASCADE
+    ALTER TABLE Employee ADD CONSTRAINT fk_Employee_ID_Rol 
+    FOREIGN KEY (ID_Rol) REFERENCES Rol(ID_Rol) 
+    ON DELETE CASCADE;
+
+-- Modificar la clave foranea en la tabla Employee para usar ON UPDATE CASCADE //Department
+    ALTER TABLE Employee ADD CONSTRAINT fk_Employee_ID_Department
+    FOREIGN KEY (ID_Department) REFERENCES Department(ID_Department) 
+    ON UPDATE CASCADE;
+
+-- Modificar la clave foranea en la tabla Employee para usar ON DELETE CASCADE
+    ALTER TABLE Employee ADD CONSTRAINT fk_Employee_ID_Department
+    FOREIGN KEY (ID_Department) REFERENCES Department(ID_Department) 
+    ON DELETE CASCADE;
+
+
+
+-- Modificar la clave foranea en la tabla Comment para usar ON UPDATE CASCADE //Ticket
+    ALTER TABLE Comment ADD CONSTRAINT fk_Comment_ID_Ticket
+    FOREIGN KEY (ID_Ticket) REFERENCES  Ticekt (ID_Ticket) 
+    ON UPDATE CASCADE;
+
+-- Modificar la clave foranea en la tabla Comment para usar ON DELETE CASCADE
+    ALTER TABLE Comment ADD CONSTRAINT fk_Comment_ID_Ticket
+    FOREIGN KEY (ID_Ticket) REFERENCES  Ticekt (ID_Ticket) 
+    ON DELETE CASCADE;
