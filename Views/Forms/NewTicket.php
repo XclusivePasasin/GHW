df52fedf448b027fbf930de750a527af5be740fd
A_Luna <alondra.luna20@itca.edu.sv>
2024-05-29 22:02:07 -0600

    NEW TICKET V2

--- Views/Forms/NewTicket.php
+++ Views/Forms/NewTicket.php
@@ -1,112 +1,102 @@
-<?php 
-	require_once 'C:\xampp\htdocs\GHW-PROJECT\Config\Connection.php';
-	$Connection = new Connection();
+<?php
+require_once 'C:\xampp\htdocs\GHW-PROJECT\Config\Connection.php';
+$Connection = new Connection();
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head lang="es">
-		<!--Import MainHead-->
-		<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/GHW-PROJECT/Views/Moduls/Head/MainHead.php'; ?>
+    <!--Import MainHead-->
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/GHW-PROJECT/Views/Moduls/Head/MainHead.php'; ?>
 </head>
+
 <body>
-        <header class="section-header">
-            <div class="tbl">
-                <div class="tbl-row">
-                    <div class="tbl-cell">
-                        <h3>NEW TICKET</h3>
-                        <ol class="breadcrumb breadcrumb-simple">
-                            <li><a href="#">CodeLab</a></li>
-                            <li><a href="#">Tickets</a></li>
-                            <li class="active">New Ticket</li>
-                        </ol>
-                    </div>
+    <header class="section-header">
+        <div class="tbl">
+            <div class="tbl-row">
+                <div class="tbl-cell">
+                    <h3>NEW TICKET</h3>
+                    <ol class="breadcrumb breadcrumb-simple">
+                        <li><a href="#">CodeLab</a></li>
+                        <li><a href="#">Tickets</a></li>
+                        <li class="active">New Ticket</li>
+                    </ol>
                 </div>
             </div>
-        </header>
-        <section class="card">
-            <div class="card-block">
-                <div class="row m-t-lg">
-                    <div class="col-md-6">
-                        <form id="form-signup_v1" name="form-signup_v1" method="POST">
-                            <div class="form-group">
-                                <label class="form-label" for="signup_v1-title">Title</label>
-                                <div class="form-control-wrapper">
-                                    <input type="text" class="form-control" id="" placeholder="Text">                                
-                                </div>
+        </div>
+    </header>
+    <section class="card">
+        <div class="card-block">
+            <div class="row m-t-lg">
+                <div class="col-md-6">
+                    <form id="form-signup_v1" name="form-signup_v1" method="POST">
+                        <div class="form-group">
+                            <label class="form-label" for="signup_v1-title">Title</label>
+                            <div class="form-control-wrapper">
+                                <input type="text" class="form-control" id="" placeholder="Subject..." required>
                             </div>
-                            <div class="form-group">
-                                <label class="form-label" for="signup_v1-user">User</label>
-                                <div class="form-control-wrapper">
-                                    <input type="text" class="form-control" id="" placeholder="Text">    
-                                </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="signup_v1-user">User</label>
+                            <div class="form-control-wrapper">
+                                <input type="text" class="form-control" id="" placeholder="Example: Elena Gomez" required>
                             </div>
-                            <div class="form-group">
-                                <label class="form-label" for="signup_v1-description">Description</label>
-                                <div class="col-sm-16">
-                                     <textarea rows="8" class="form-control" placeholder="Textarea"></textarea>
-                                </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="signup_v1-description">Description</label>
+                            <div class="col-sm-16">
+                                <textarea rows="8" class="form-control" placeholder="Description of problem" required></textarea>
                             </div>
-                            <div class="form-group">
-                                <label class="form-label" for="signup_v1-categoryl">Category</label>
-                                <select id="exampleSelect" class="form-control">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn">Send</button>
-                            </div>
-                        </form>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="signup_v1-category">Category</label>
+                            <select id="category" class="form-control" required>
+                                <option hidden>Select category</option>
+                                <option value=""></option>
+                            </select>
+                        </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label class="form-label" for="signup_v2-name">Nª Ticekt</label>
+                        <div class="form-control-wrapper">
+                            <input type="number" class="form-control" id="" placeholder="Nª Ticekt" disabled required>
+                        </div>
                     </div>
-                    <div class="col-md-6">
-                        <form id="form-signup_v2" name="form-signup_v2" method="POST">
-                            <div class="form-group">
-                                <label class="form-label" for="signup_v2-name">Nª Ticekt</label>
-                                <div class="form-control-wrapper">
-                                    <input id="signup_v2-name" class="form-control" name="signup_v2[name]" type="text" data-validation="[OPTIONAL, NAME, L>=2, TRIM]">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="form-label" for="signup_v2-name">Email</label>
-                                <div class="form-control-wrapper">
-                                    <input id="signup_v2-name" class="form-control" name="signup_v2[name]" type="text" data-validation="[OPTIONAL, NAME, L>=2, TRIM]">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="form-label" for="signup_v1-categoryl">Problems</label>
-                                <select id="exampleSelect" class="form-control">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label class="form-label" for="signup_v1-categoryl">Priority</label>
-                                <select id="exampleSelect" class="form-control">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label class="form-label" for="signup_v1-categoryl">Date</label>
-                                <select id="exampleSelect" class="form-control">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                </select>
-                            </div>
-                        </form>
+                    <div class="form-group">
+                        <label class="form-label" for="signup_v2-name">Email</label>
+                        <div class="form-control-wrapper">
+                            <input name="Email" type="Email" class="form-control" placeholder="@codelab.sv" required>
+                        </div>
                     </div>
-                </div><!--.row-->
+                    <div class="form-group">
+                        <label class="form-label" for="signup_v1-problems">Problems</label>
+                        <select id="problems" class="form-control" required>
+                            <option hidden>Select Problems</option>
+                            <option value=""></option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label" for="signup_v1-priority">Priority</label>
+                        <select id="priority" class="form-control" required>
+                            <option hidden>Select Priority</option>
+                            <option value=""></option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <center>
+                        <div class="form-group">
+                            <button type="submit" class="btn">Submit ticket</button>
+                        </div>
+                    </center>
+                </div>
+            </form>
             </div>
-        </section>
-		<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/GHW-PROJECT/Views/Moduls/Head/MainJS.php'; ?>
+        </div><!--.row-->
+    </section>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/GHW-PROJECT/Views/Moduls/Head/MainJS.php'; ?>
 </body>
+
 </html>
